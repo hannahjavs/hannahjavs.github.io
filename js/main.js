0215601b29d8a44917df71f83b33395387d763c9
hannahjavs <hannahsiujadavji@gmail.com>
2017-11-27 17:53:01 +0000

    Javascript updated - bug fixed

--- js/main.js
+++ js/main.js
@@ -23,29 +23,28 @@ $(() => {
         });
       } // End if
     });
+
+    // AUTO TYPING
+    $('#autotype').typeIt({
+      speed: 50,
+      autoStart: false,
+      loop: true
+    })
+      .tiType('Hll')
+      .tiPause(500)
+      .tiDelete(2)
+      .tiType('ello, ')
+      .tiPause(1000)
+      .tiType('I\'m Hannah Jadavji,')
+      .tiBreak() .tiPause(750)
+      .tiType(' a full stack web developer')
+      .tiSettings({speed: 700})
+      .tiType('...')
+      .tiPause(750)
+      .tiSettings({speed: 50})
+      .tiDelete()
+      .tiType('Click a link below to see me <strong>development</strong> work');
+
   });
-
-
-  // <p id="example4"></p>
-
-  $('#example4').typeIt({
-    speed: 50,
-    autoStart: false
-  })
-    .tiType('Wll')
-    .tiPause(500)
-    .tiDelete(2)
-    .tiType('ell, ')
-    .tiPause(1000)
-    .tiType('I guess I\'m typing..')
-    .tiBreak() .tiPause(750)
-    .tiType(' but I don\'t really know what to say')
-    .tiSettings({speed: 700})
-    .tiType('...')
-    .tiPause(750)
-    .tiSettings({speed: 50})
-    .tiDelete()
-    .tiType('IS THAT SO <strong>WRONG??</strong>');
-
 
 });
