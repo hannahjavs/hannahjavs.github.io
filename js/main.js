91d72fb575c118c0c5d6a777e2584dd9e114c529
hannahjavs <hannahsiujadavji@gmail.com>
2017-11-27 17:01:16 +0000

    Typing javascript added to heading

--- js/main.js
+++ js/main.js
@@ -1,11 +1,9 @@
-
 $(() => {
-  console.log('this is jquery');
+  console.log('this is jquery and im loaded');
 
   $(document).ready(function(){
     // Add smooth scrolling to all links
     $('a').on('click', function(event) {
-
       // Make sure this.hash has a value before overriding default behavior
       if (this.hash !== '') {
         // Prevent default anchor click behavior
@@ -26,4 +24,28 @@ $(() => {
       } // End if
     });
   });
+
+
+  // <p id="example4"></p>
+
+  $('#example4').typeIt({
+    speed: 50,
+    autoStart: false
+  })
+    .tiType('Wll')
+    .tiPause(500)
+    .tiDelete(2)
+    .tiType('ell, ')
+    .tiPause(1000)
+    .tiType('I guess I\'m typing..')
+    .tiBreak() .tiPause(750)
+    .tiType(' but I don\'t really know what to say')
+    .tiSettings({speed: 700})
+    .tiType('...')
+    .tiPause(750)
+    .tiSettings({speed: 50})
+    .tiDelete()
+    .tiType('IS THAT SO <strong>WRONG??</strong>');
+
+
 });
